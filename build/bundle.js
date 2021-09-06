0cbe934a1b79bde2cabf425e2b9e9162f89d27ff
KingArthur-IT <67517973+KingArthur-IT@users.noreply.github.com>
2021-09-06 10:54:56 +0300

    mobile test

--- build/bundle.js
+++ build/bundle.js
@@ -44302,13 +44302,13 @@
 	};
 
 	let objectsParams = {
-		modelPath: './assets/Models/',
+		modelPath: './assets/models/',
 		bovie: {
 			bovieObj: 'bovie_pen_01.obj',
 			bovieMtl: 'bovie_pen_01.mtl',
 			scale: new Vector3(2.0, 2.0, 2.0),
 			position: new Vector3(0.0, 2.0, 0.0),
-			rotation: new Vector3(70.0 * Math.PI / 180.0,
+			rotation: new Vector3(80.0 * Math.PI / 180.0,
 				-220.0 * Math.PI / 180.0, 0.0)
 		},
 		line: {
@@ -44326,6 +44326,12 @@
 			maxOffset: 0.5,
 			perspectiveEdit: 3.0
 		}
+	};
+
+	let touchParams = {
+		objectCenter: { x: 425.0, y: 20.0 },
+		radius: 50,
+		limits: {min: 0.0, max: 450.0}
 	};
 
 	class App {
@@ -44389,6 +44395,10 @@
 			canvas.addEventListener('mousedown', onMouseDown, false);
 			popupBtn.addEventListener('click', removePopup, false);
 
+			canvas.addEventListener("touchstart",   touch_start_handler);
+	    	canvas.addEventListener("touchmove",    touch_move_handler);    
+	    	canvas.addEventListener("touchend",     touch_up_handler);
+
 			animate();
 		}
 	}
@@ -44409,60 +44419,67 @@
 			let newYPosition = bovieObj.position.y - movementY * params.positionProps.step;
 			if (newYPosition < params.positionProps.maxY && newYPosition > params.positionProps.minY) {
 				bovieObj.position.y -= movementY * params.positionProps.step;
-				//upper line
-				if (bovieObj.position.y > objectsParams.line.lineLimits.upper.bottom &&
-					bovieObj.position.y < objectsParams.line.lineLimits.upper.top) {
-					if (bovieObj.position.y > currentLineLimits.upper.top ||
-						currentLineLimits.upper.top === 100)
-						currentLineLimits.upper.top = bovieObj.position.y;
-					if (bovieObj.position.y < currentLineLimits.upper.bottom ||
-						currentLineLimits.upper.bottom === 100)
-						currentLineLimits.upper.bottom = bovieObj.position.y;
-				}
-				//lower line
-				if (bovieObj.position.y > objectsParams.line.lineLimits.lower.bottom &&
-					bovieObj.position.y < objectsParams.line.lineLimits.lower.top) {
-					if (bovieObj.position.y > currentLineLimits.lower.top ||
-						currentLineLimits.lower.top === 100)
-						currentLineLimits.lower.top = bovieObj.position.y;
-					if (bovieObj.position.y < currentLineLimits.lower.bottom ||
-						currentLineLimits.lower.bottom === 100) {
-							currentLineLimits.lower.bottom = bovieObj.position.y;
-						}
-				}
+
 				bovieObj.rotation.x += movementY * params.rotationProps.step;
 				let newYAngle = bovieObj.rotation.y + movementX * params.rotationProps.step;
 				if (newYAngle > params.rotationProps.minXAngle && newYAngle < params.rotationProps.maxXAngle)
 					bovieObj.rotation.y += movementX * params.rotationProps.step;
-				//line
-				scene.remove(upperLine);
-				scene.remove(lowerLine);
-
-				let upperGeometry = new LineGeometry();
-				let lowerGeometry = new LineGeometry();
-
-				let posArray = [
-					objectsParams.bovie.position.x,
-					objectsParams.bovie.position.y,
-					objectsParams.bovie.position.z,
-					objectsParams.bovie.position.x,
-					objectsParams.bovie.position.y,
-					objectsParams.bovie.position.z + objectsParams.line.perspectiveEdit];
-
-				posArray[1] = currentLineLimits.upper.top + objectsParams.bovie.position.y;
-				posArray[4] = currentLineLimits.upper.bottom + objectsParams.bovie.position.y;
-				upperGeometry.setPositions(posArray);
-
-				posArray[1] = currentLineLimits.lower.top + objectsParams.bovie.position.y;
-				posArray[4] = currentLineLimits.lower.bottom + objectsParams.bovie.position.y;
-				lowerGeometry.setPositions(posArray);
-
-				upperLine = new Line2(upperGeometry, lineMtl);
-				lowerLine = new Line2(lowerGeometry, lineMtl);
-				scene.add(upperLine);
-				scene.add(lowerLine);
+				
+				DrawLine();
 			}
 		}
+	}
+
+	function DrawLine() {
+		//upper line
+		if (bovieObj.position.y > objectsParams.line.lineLimits.upper.bottom &&
+			bovieObj.position.y < objectsParams.line.lineLimits.upper.top) {
+			if (bovieObj.position.y > currentLineLimits.upper.top ||
+				currentLineLimits.upper.top === 100)
+				currentLineLimits.upper.top = bovieObj.position.y;
+			if (bovieObj.position.y < currentLineLimits.upper.bottom ||
+				currentLineLimits.upper.bottom === 100)
+				currentLineLimits.upper.bottom = bovieObj.position.y;
+		}
+		//lower line
+		if (bovieObj.position.y > objectsParams.line.lineLimits.lower.bottom &&
+			bovieObj.position.y < objectsParams.line.lineLimits.lower.top) {
+			if (bovieObj.position.y > currentLineLimits.lower.top ||
+				currentLineLimits.lower.top === 100)
+				currentLineLimits.lower.top = bovieObj.position.y;
+			if (bovieObj.position.y < currentLineLimits.lower.bottom ||
+				currentLineLimits.lower.bottom === 100) {
+					currentLineLimits.lower.bottom = bovieObj.position.y;
+				}
+		}
+		
+		//line
+		scene.remove(upperLine);
+		scene.remove(lowerLine);
+
+		let upperGeometry = new LineGeometry();
+		let lowerGeometry = new LineGeometry();
+
+		let posArray = [
+			objectsParams.bovie.position.x,
+			objectsParams.bovie.position.y,
+			objectsParams.bovie.position.z,
+			objectsParams.bovie.position.x,
+			objectsParams.bovie.position.y,
+			objectsParams.bovie.position.z + objectsParams.line.perspectiveEdit];
+
+		posArray[1] = currentLineLimits.upper.top + objectsParams.bovie.position.y;
+		posArray[4] = currentLineLimits.upper.bottom + objectsParams.bovie.position.y;
+		upperGeometry.setPositions(posArray);
+
+		posArray[1] = currentLineLimits.lower.top + objectsParams.bovie.position.y;
+		posArray[4] = currentLineLimits.lower.bottom + objectsParams.bovie.position.y;
+		lowerGeometry.setPositions(posArray);
+
+		upperLine = new Line2(upperGeometry, lineMtl);
+		lowerLine = new Line2(lowerGeometry, lineMtl);
+		scene.add(upperLine);
+		scene.add(lowerLine);
 	}
 
 	function onMouseDown() {
@@ -44474,22 +44491,7 @@
 				document.webkitExitPointerLock;
 			document.exitPointerLock();
 			params.isBovieLocked = false;
-			//check
-			if (Math.abs(currentLineLimits.upper.top - objectsParams.line.lineLimits.upper.top) < objectsParams.line.maxOffset &&
-				Math.abs(currentLineLimits.upper.bottom - objectsParams.line.lineLimits.upper.bottom) < objectsParams.line.maxOffset &&
-				Math.abs(currentLineLimits.lower.top - objectsParams.line.lineLimits.lower.top) < objectsParams.line.maxOffset &&
-				Math.abs(currentLineLimits.lower.bottom - objectsParams.line.lineLimits.lower.bottom) < objectsParams.line.maxOffset) {
-				params.isSuccess = true;
-				setTimeout(() => {
-					addPopup();
-				}, 1000);
-			}
-			else {
-				params.isSuccess = false;
-				setTimeout(() => {
-					addPopup();
-				}, 1000);
-			}
+			CheckDrawedLine();
 		}
 		else {
 			//lock
@@ -44498,6 +44500,24 @@
 				canvas.webkitRequestPointerLock;
 			canvas.requestPointerLock();
 			params.isBovieLocked = true;
+		}
+	}
+
+	function CheckDrawedLine() {
+		if (Math.abs(currentLineLimits.upper.top - objectsParams.line.lineLimits.upper.top) < objectsParams.line.maxOffset &&
+			Math.abs(currentLineLimits.upper.bottom - objectsParams.line.lineLimits.upper.bottom) < objectsParams.line.maxOffset &&
+			Math.abs(currentLineLimits.lower.top - objectsParams.line.lineLimits.lower.top) < objectsParams.line.maxOffset &&
+			Math.abs(currentLineLimits.lower.bottom - objectsParams.line.lineLimits.lower.bottom) < objectsParams.line.maxOffset) {
+			params.isSuccess = true;
+			setTimeout(() => {
+				addPopup();
+			}, 1000);
+		}
+		else {
+			params.isSuccess = false;
+			setTimeout(() => {
+				addPopup();
+			}, 1000);
 		}
 	}
 
@@ -44559,6 +44579,53 @@
 		popupBtn.style.display = 'none';
 		if (!params.isSuccess) {
 			onMouseDown();
+		}
+	}
+
+	function touch_start_handler(e) {
+		if (!params.isActive) return;
+		let evt = (typeof e.originalEvent === 'undefined') ? e : e.originalEvent;
+		let touch = evt.touches[0] || evt.changedTouches[0];
+		
+		let dist = (parseInt(touch.pageX) - touchParams.objectCenter.x) *
+			(parseInt(touch.pageX) - touchParams.objectCenter.x) +
+			(parseInt(touch.pageY) - touchParams.objectCenter.y) *
+			(parseInt(touch.pageY) - touchParams.objectCenter.y);
+		if (Math.sqrt(dist) < touchParams.radius) {
+			params.isBovieLocked = true;
+		}
+	}
+
+	function touch_move_handler(e) {
+		let evt = (typeof e.originalEvent === 'undefined') ? e : e.originalEvent;
+		let touch = evt.touches[0] || evt.changedTouches[0];
+		if (params.isBovieLocked) {
+			let newMouseY = params.sceneHeight - parseInt(touch.pageY);
+			if (newMouseY < touchParams.limits.max && newMouseY > touchParams.limits.min) {
+				let newYPosition = (newMouseY - touchParams.limits.min) *
+					(params.positionProps.maxY - params.positionProps.minY) /
+					(touchParams.limits.max - touchParams.limits.min) + params.positionProps.minY;
+				bovieObj.position.y = newYPosition;
+				
+				let squeeze = 0.5;
+				bovieObj.rotation.x = (10.0 * touch.pageY / touchParams.limits.max) * Math.PI / 180.0;
+				let newYAngle = bovieObj.rotation.y + (touch.pageX - 0.5 * params.sceneWidth) / 100.0;
+				if (newYAngle > squeeze * params.rotationProps.minXAngle && newYAngle < squeeze * params.rotationProps.maxXAngle)
+				{
+					bovieObj.rotation.y = (touch.pageX - 0.5 * params.sceneWidth) / 200.0;
+					touchParams.objectCenter.y = parseInt(touch.pageY);
+					touchParams.objectCenter.x = parseInt(touch.pageX);
+				}
+				
+				DrawLine();
+			}
+		}
+	}
+
+	function touch_up_handler(e) {	
+		if (params.isBovieLocked) {
+			CheckDrawedLine();
+			params.isBovieLocked = false;
 		}
 	}
 
